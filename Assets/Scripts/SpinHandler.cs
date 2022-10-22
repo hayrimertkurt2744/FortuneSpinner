5a3a9e714498628e7c2352e1fedc837c7148f7c9
Hayri Mert Kurt <hayrimertkurt@outlook.com>
2022-10-22 09:57:30 +0300

    SDK settings

--- Assets/Scripts/SpinHandler.cs
+++ Assets/Scripts/SpinHandler.cs
@@ -20,18 +20,20 @@ public class SpinHandler : MonoBehaviour
     {
         
         GameManager.Instance.currentState = GameManager.GameState.Spinning;
+
             spinnerTransform.DORotate(new Vector3(0, 0, 360+360+GameManager.Instance.AngleListSO.angleList[Random.Range(0, GameManager.Instance.AngleListSO.angleList.Count - 1)]), 
                 3,RotateMode.FastBeyond360).OnComplete(() =>
                 {
                     GameManager.onWinEvent?.Invoke();
-                    GameManager.Instance.currentState = GameManager.GameState.AfterSpin;
+                    //GameManager.Instance.currentState = GameManager.GameState.AfterSpin;
+                    GameManager.Instance.currentState = GameManager.GameState.Normal;
 
 
                     //GameManager.Instance.AngleListSO.angleList[chosenSpeenAngle]=0;
                     //if the choosen angle's prize's amount is zero, control it then open a new type of uý 
 
                 });
-        GameManager.Instance.currentState = GameManager.GameState.Normal;
+        
         //Wait for 4 seconds
         yield return new WaitForSeconds(4);
 
